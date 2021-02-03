b7b0f6d3811335a31428d7a4626a4f5b85406e64
randomblame <dumbandroid@gmail.com>
2021-02-02 22:45:02 -0800

    change z offset for new blt mount

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1079,7 +1079,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -40, -12, -5.47 }
+#define NOZZLE_TO_PROBE_OFFSET { -40, -12, -2.89 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
