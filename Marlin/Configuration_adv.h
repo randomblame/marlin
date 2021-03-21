71ac4987e8becb2f644bae7f6a575ad69489594f
randomblame <dumbandroid@gmail.com>
2021-03-21 16:20:14 -0700

    Move tramming points to be reachable with probe offset

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -838,7 +838,7 @@
 #if ENABLED(ASSISTED_TRAMMING)
 
   // Define positions for probe points.
-  #define TRAMMING_POINT_XY { {  20, 20 }, { 180,  20 }, { 180, 180 }, { 20, 180 } }
+  #define TRAMMING_POINT_XY { {  40, 20 }, { 180,  20 }, { 180, 180 }, { 40, 180 } }
 
   // Define position names for probe points.
   #define TRAMMING_POINT_NAME_1 "Front-Left"
